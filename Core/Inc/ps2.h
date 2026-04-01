e9948932c080a4ac5fe4173dc9da66d581b7f2bd
RobConfor <122578818+RobConfor@users.noreply.github.com>
2026-04-01 12:19:44 -0400

    Refactor PS2 read_ps2 function declaration
    Removed the implementation of read_ps2 function and kept its declaration.

--- Core/Inc/ps2.h
+++ Core/Inc/ps2.h
@@ -43,19 +43,4 @@
 
 extern SPI_HandleTypeDef hspi3;
 
-uint8_t PS2_RX[9];
-uint8_t PS2_TX[9] = { 0x01, 0x42};
-
-void read_ps2(uint8_t *x, uint8_t *y){
-
-    // initiate transaction with PS2 controller
-	HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_6);
-	HAL_SPI_TransmitReceive(&hspi3, PS2_TX, PS2_RX, 9, 10);
-	HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_6);
-
-    // read RX
-    if(PS2_RX[5] != 0x7b || PS2_RX[6] != 0x7b){
-        *x = PS2_RX[5];
-        *y = PS2_RX[6];
-    }
-}
+void read_ps2(uint8_t *x, uint8_t *y);
