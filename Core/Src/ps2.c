88dcef6aa08d7eea38cde0ce9b60e14ae01e2989
RobConfor <122578818+RobConfor@users.noreply.github.com>
2026-04-01 12:19:14 -0400

    ps2.c
    Implement PS2 controller read function to handle input.

---
+++ Core/Src/ps2.c
@@ -0,0 +1,15 @@
+#include "p2s.h"
+
+void read_ps2(uint8_t *x, uint8_t *y){
+
+    // initiate transaction with PS2 controller
+	HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_6);
+	HAL_SPI_TransmitReceive(&hspi3, PS2_TX, PS2_RX, 9, 10);
+	HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_6);
+
+    // read RX
+    if(PS2_RX[5] != 0x7b || PS2_RX[6] != 0x7b){
+        *x = PS2_RX[5];
+        *y = PS2_RX[6];
+    }
+}
