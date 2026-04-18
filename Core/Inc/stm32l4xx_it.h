38c37c6fe34d0b095c06ccfe09c8320145a185cd
Reshob Islam <reshobi@umich.edu>
2026-04-18 15:36:29 -0400

    Added EXTI15_10_IRQHandler function prototype

--- Core/Inc/stm32l4xx_it.h
+++ Core/Inc/stm32l4xx_it.h
@@ -59,6 +59,7 @@ void DMA1_Channel1_IRQHandler(void);
 void DMA1_Channel2_IRQHandler(void);
 void I2C1_EV_IRQHandler(void);
 void I2C2_EV_IRQHandler(void);
+void EXTI15_10_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
